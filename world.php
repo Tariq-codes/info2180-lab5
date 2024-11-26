f06adf2823c5432101cc3083683dd08a4088dd70
Tariq Davy <davytariq@gmail.com>
2024-11-25 19:51:45 -0500

    Exercise 4

--- world.php
+++ world.php
@@ -28,12 +28,29 @@ $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<ul>
+
 <?php if (count($results) > 0): ?>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Country Name</th>
+                <th>Continent</th>
+                <th>Independence Year</th>
+                <th>Head of State</th>
+            </tr>
+        </thead>
+    <tbody>
     <?php foreach ($results as $row): ?>
-        <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+        <tr>
+            <td><?=htmlspecialchars($row['name']); ?></td>
+            <td><?=htmlspecialchars($row['continent']);?></td>
+            <td><?=htmlspecialchars($row['independence_year']);?></td>
+            <td><?=htmlspecialchars($row['head_of_state']);?></td>
+        </tr>
     <?php endforeach; ?>
+    </tbody>
+    </table>
 <?php else: ?>
     <li>No countries found matching the search.</li>
 <?php endif; ?>
-</ul>
+
